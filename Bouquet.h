bdadcde9ccfbb83bc5297713045ee2ad5bb38215
JoLerray <68341593+JoLerray@users.noreply.github.com>
2021-06-04 10:29:06 +0700

    Update Bouquet.h

--- Bouquet.h
+++ Bouquet.h
@@ -5,22 +5,21 @@ class Bouquet {
 private:
 	vector <Flower> flowers;
 public:
-	Bouquet(int n = 0, Flower F = Flower(),...);
-	void addFlower(Flower);
-	void addFlower(vector <Flower>);
-	void addFlower(int,Flower,...);
-	void delFlower(int);
-	void delFlower();
-	string listFlowers();
-	float getState();
-	//bool getInWater();
-	float getCost();
-	void inBottle();
-	void outBottle();
-	int realyCost();
-	int countFlowers();
-	int countWiltedFlowers();
-	void changeTime(int);
-	~Bouquet();
+	Bouquet(int n = 0, Flower F = Flower(),...); // Конструктор
+	void addFlower(Flower);// Добавление 1 цветка в букет
+	void addFlower(vector <Flower>); // Добавление букета в букет
+	void addFlower(int,Flower,...); // Добавление несколько цветков в букет
+	void delFlower(int); // Удаление цветка из букета по индексу
+	void delFlower(); // Удаление последнего цветка
+	string listFlowers(); // Вывод списка цветков
+	float getState(); // Состояние букета
+	float getCost(); // Базовая стоимость букета
+	void inBottle(); // Поставить букет в воду
+	void outBottle(); // Убрать букет из воды 
+	int realyCost(); // Стоимость букета с учётом времени
+	int countFlowers(); // Количество цветов в букете
+	int countWiltedFlowers(); // Количество цветов которые завяли в букете
+	void changeTime(int); // Изменение времени букета
+	~Bouquet();// Деструктор
 	
 };
